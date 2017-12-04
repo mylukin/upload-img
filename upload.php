664e55ec8a8cc3bc3d8a1222ca8fc2bf0988d0f9
Lukin <luxiansheng@mogoroom.com>
2017-12-04 16:53:01 +0800

    first commit

---
+++ upload.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukin
+ * Date: 04/12/2017
+ * Time: 15:14
+ */
+include __DIR__ . '/vendor/autoload.php';
+
+use OSS\OSSClient;
+use OSS\Core\OSSException;
+
+$response = [
+    'status' => 'ok',
+];
+header('Content-Type: application/json');
+
+if (isset($_POST['filedata'])) {
+    $file_data = $_POST['filedata'];
+    $file_data_arr = explode(',', $file_data);
+    $file_type = $file_data_arr[0];
+    $file_data = $file_data_arr[1];
+    if (preg_match('@data:image\/(\w+);base64@i', $file_type, $args)) {
+        $file_ext = $args[1];
+        $file_data = base64_decode($file_data);
+    } else {
+        $response = [
+            'status' => 'err',
+            'message' => 'file type error',
+        ];
+        echo json_encode($response);
+        exit;
+    }
+
+} else {
+    $response = [
+        'status' => 'err',
+        'message' => 'no data',
+    ];
+    echo json_encode($response);
+    exit;
+}
+
+
+$accessKeyId = "LTAItYZgeQvwSThi";
+$accessKeySecret = "tFtDfNRuDEKJRskikVRTtpNaclS1PL";
+$endpoint = "oss-cn-hangzhou.aliyuncs.com";
+$folder = isset($_POST['folder']) ? $_POST['folder'] : '';
+try {
+    $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint);
+    $bucket = "mogo-static-files";
+    $object = sprintf('%s/%s.%s', $folder, md5($file_data), $file_ext);
+    $content = $file_data;
+    try {
+        $file_info = $ossClient->putObject($bucket, $object, $content);
+        $response['data'] = $file_info;
+        $response['path'] = $object;
+
+    } catch (OssException $e) {
+        $response = [
+            'status' => 'err',
+            'message' => $e->getMessage(),
+        ];
+        echo json_encode($response);
+        exit;
+    }
+} catch (OssException $e) {
+    $response = [
+        'status' => 'err',
+        'message' => $e->getMessage(),
+    ];
+    echo json_encode($response);
+    exit;
+}
+
+
+echo json_encode($response);
